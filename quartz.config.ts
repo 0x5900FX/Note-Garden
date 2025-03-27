52ab80de2bdee39cd5529497a2c8bad46fa583f7
0x5900FX <404niraj@gmail.com>
2025-03-28 00:20:01 +0545

    Quartz sync: Mar 28, 2025, 12:20 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "MrXcrypt",
+    pageTitle: "0x5900FX",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
